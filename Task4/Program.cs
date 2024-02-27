e18faccfe25fb6484ca70a91dcadae44fd4ed1ed
Max <Max>
2024-02-27 19:05:15 +0300

    4th Task Complete

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿void Main(){
+
+    string myString = "Hello there !";
+    var array = myString.Split();
+    Array.Reverse(array);
+    System.Console.WriteLine(string.Join(" ", array));
+
+}
+
+
+
+Main();

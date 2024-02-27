d9b608b443698f0c047f8b01eaf212a87593ff8e
Max <Max>
2024-02-27 18:30:43 +0300

    1st Task Complete

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿void Main()
+{
+    char[,] symbols = 
+    {
+    {'a', 'b', 'c', 'd'},
+    {'e', 'f', 'g', 'h'}
+    };
+
+    string myString = makeMyString(symbols); 
+    PrintResult(myString);
+}
+
+string makeMyString(char[,] array){
+
+    string str = "";
+
+    for(int i = 0;i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            str += array[i,j];
+        }
+    }
+    return str;
+}
+
+void PrintResult(string str){
+    System.Console.WriteLine(str);
+}
+
+Main();

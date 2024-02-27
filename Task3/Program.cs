e65ea3409f1be80e1352edc0b8fca4d39b5abdd9
Max <Max>
2024-02-27 18:55:21 +0300

    3rd Task Complete

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿void Main ()
+{
+    string myString = "sghnojs";
+    
+    System.Console.WriteLine(IsPalindrome(myString));
+}
+
+bool IsPalindrome(string str){
+    char[] charArray = str.ToCharArray();
+    Array.Reverse(charArray);
+
+    return string.Equals(str, new string(charArray));
+}
+
+Main();

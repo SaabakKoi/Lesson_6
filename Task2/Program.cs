4d6fe8ef5d1c763194be36baf28c016b89e87729
Max <Max>
2024-02-27 18:37:36 +0300

    2nd Task Complete

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿void Main(){
+
+    string str1 = "GNkfkbLKNG";
+    System.Console.WriteLine(str1);
+    System.Console.WriteLine();
+    string str2 = str1.ToLower();
+    System.Console.WriteLine(str2);
+
+}
+
+Main();
